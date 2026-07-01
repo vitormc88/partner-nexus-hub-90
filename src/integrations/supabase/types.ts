89a62c5605e416d75227c8ab134efd962b7e7fe3
Vitor Carvalho <vitormc88@gmail.com>
2026-07-01 15:20:03 +0100

    Revert "Swapped favicon to uploaded icon"
    This reverts commit 92d2cefbe30562673d94a63cd5fe258b0b69a1c6, reversing
    changes made to eef701ff2f870026a9e4e9652273c816bea5872f.

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -4532,7 +4532,6 @@ export type Database = {
         Args: { message_id: number; queue_name: string }
         Returns: boolean
       }
-      email_queue_dispatch: { Args: never; Returns: undefined }
       enqueue_email: {
         Args: { payload: Json; queue_name: string }
         Returns: number
